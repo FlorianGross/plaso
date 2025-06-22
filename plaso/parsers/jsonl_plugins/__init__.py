98010fbda6bb54d6e96ef67937c08092e1816174
Florian Groß <floriangross@hotmail.de>
2025-06-23 00:24:53 +0200

    Fixed Parsers

--- plaso/parsers/jsonl_plugins/__init__.py
+++ plaso/parsers/jsonl_plugins/__init__.py
@@ -11,5 +11,4 @@ from plaso.parsers.jsonl_plugins import gcp_log
 from plaso.parsers.jsonl_plugins import ios_app_privacy
 from plaso.parsers.jsonl_plugins import microsoft365_audit_log
 from plaso.parsers.jsonl_plugins import microsoft365_ual
-from plaso.parsers.jsonl_plugins import microsoft365_signin_log
 from plaso.parsers.jsonl_plugins import microsoft365_entra_audit_log
